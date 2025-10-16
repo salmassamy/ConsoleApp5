ed3ecf6681960a848e23b3c36ea57eeabbc8b4ed
salmassamy <ssamy6675@gmail.com>
2025-10-16 21:05:12 +0300

    Add project files.

---
+++ ConsoleApp5/IAuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    #region Question02
+    public interface IAuthenticationService
+    {
+        bool AuthenticateUser(string username, string password);
+        bool AuthorizeUser(string username, string role);
+    }
+
+    public class BasicAuthenticationService : IAuthenticationService
+    {
+        private readonly string storedUsername = "admin";
+        private readonly string storedPassword = "1234";
+        private readonly string storedRole = "Administrator";
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            return username == storedUsername && password == storedPassword;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            return username == storedUsername && role == storedRole;
+        }
+    }
+}
+#endregion
+
